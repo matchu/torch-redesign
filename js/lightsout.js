04f72d4d85b755dc924c26ea02a212a2f3ff8acf
Zackary Corbett <Zackary@new-host-2.home>
2011-10-04 20:12:23 -0400

    Fancy jQuery magic to improve site stuff.

---
+++ js/lightsout.js
@@ -0,0 +1,27 @@
+$(window).load(function() {
+	if(($('#content').height()- $(window).height()) > 100) {
+	/*If the hight of the story minus the hight of the aticle is greater than 100 pixels, then implement show on scroll to bottom functionality*/
+		$(window).scroll(function() {
+		   if($(window).scrollTop() + $(window).height() > $(document).height() - 50) {
+			   if($('#header').is(':hidden')) {
+			   	$('#header').fadeIn();
+			   	$('#more').fadeIn();
+			   	$('#lights').text('Lights Out');
+			   }
+		   }
+		   /*Code Via http://stackoverflow.com/questions/3898130/how-to-check-if-a-user-has-scrolled-to-the-bottom/3898152#3898152*/
+		});
+	}
+});
+
+function flip_switch() {
+	if($('#header').is(':hidden')) {
+		$('#header').fadeIn();
+		$('#more').fadeIn();
+		$('#lights').text('Lights Out');
+	}else {
+		$('#header').fadeOut();
+		$('#more').fadeOut();
+		$('#lights').text('Lights On');
+	}
+}
